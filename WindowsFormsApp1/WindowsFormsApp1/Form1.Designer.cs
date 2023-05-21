d96b4355bba40545a9b19df546dbe33cf7697499
sh9311 <65700579+sh9311@users.noreply.github.com>
2023-05-21 13:24:29 +0530

    Add files via upload
    Library management using ADO.net

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trainingDataSet1 = new WindowsFormsApp1.TrainingDataSet1();
+            this.trainingDataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bookid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookprice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trainingDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trainingDataSet1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(409, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Library Management System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Book Id";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(583, 278);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter details";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(453, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Edit Record";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(233, 188);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(453, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(233, 134);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "View All";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(233, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(453, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(233, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Book Price";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Student Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Book Name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // trainingDataSet1
+            // 
+            this.trainingDataSet1.DataSetName = "TrainingDataSet1";
+            this.trainingDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // trainingDataSet1BindingSource
+            // 
+            this.trainingDataSet1BindingSource.DataSource = this.trainingDataSet1;
+            this.trainingDataSet1BindingSource.Position = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.bookid,
+            this.bookname,
+            this.studentname,
+            this.bookprice});
+            this.dataGridView1.DataSource = this.trainingDataSet1BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(624, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(387, 278);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // bookid
+            // 
+            this.bookid.DataPropertyName = "bookid";
+            this.bookid.HeaderText = "Id";
+            this.bookid.Name = "bookid";
+            // 
+            // bookname
+            // 
+            this.bookname.DataPropertyName = "bookname";
+            this.bookname.HeaderText = "Name";
+            this.bookname.Name = "bookname";
+            // 
+            // studentname
+            // 
+            this.studentname.DataPropertyName = "studentname";
+            this.studentname.HeaderText = "Student name";
+            this.studentname.Name = "studentname";
+            // 
+            // bookprice
+            // 
+            this.bookprice.DataPropertyName = "bookprice";
+            this.bookprice.HeaderText = "Book price";
+            this.bookprice.Name = "bookprice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1032, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trainingDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trainingDataSet1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.BindingSource trainingDataSet1BindingSource;
+        private TrainingDataSet1 trainingDataSet1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookprice;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+         
+        public void clearData()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-AT4S4F9V;Initial Catalog=Training;Integrated Security=True");
+       private BindingSource bs = new BindingSource();
+
+        public void display()
+        {
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter("Select * from library", con);
+            DataTable dt = new DataTable();
+            sqlAdapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Visible = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = false;  
+        
+
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into library values(@bookid,@bookname,@studentname,@bookprice)", con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@bookid", textBox1.Text);
+            cmd.Parameters.AddWithValue("@bookname", textBox2.Text);
+            cmd.Parameters.AddWithValue("@studentname", textBox3.Text);
+            cmd.Parameters.AddWithValue("@bookprice", textBox4.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            clearData();
+            MessageBox.Show("Book assigned to student successfully");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-AT4S4F9V;Initial Catalog=Training;Integrated Security=True"))
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("select * from library", con);
+                /* SqlCommand cm = new SqlCommand("select * from library", con);
+                  DataTable dt = new DataTable();
+
+                  con.Open();
+                  SqlDataReader rdr = cm.ExecuteReader();
+
+                  dt.Load(rdr);
+                  con.Close();
+                  dataGridView1.DataSource = dt;
+                  dataGridView1.Visible = true;*/
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                bs.DataSource = dt;
+                dataGridView1.DataSource = bs;
+                dataGridView1.Visible = true;
+               // display();
+                MessageBox.Show("all records are feteched");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+
+        {
+
+            if (textBox3.Text == null)
+            {
+                MessageBox.Show("please Enter the name of student");
+            }
+            else { 
+            SqlCommand c = new SqlCommand("delete from library where studentname=@studentname", con);
+             c.CommandType = CommandType.Text;
+             c.Parameters.AddWithValue("@studentname", textBox3.Text);
+             con.Open();
+             c.ExecuteNonQuery();
+             con.Close();
+             clearData();
+             MessageBox.Show("Book returned by student and book deleted");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand s = new SqlCommand("update library set bookid=@bookid,bookname=@bookname,studentname=@studentname,bookprice=@bookprice where studentname=@studentname", con);
+            s.CommandType = CommandType.Text;
+            s.Parameters.AddWithValue("@bookid", textBox1.Text);
+            s.Parameters.AddWithValue("@bookname", textBox2.Text);
+            s.Parameters.AddWithValue("@studentname", textBox3.Text);
+            s.Parameters.AddWithValue("@bookprice", textBox4.Text);
+            con.Open();
+            s.ExecuteNonQuery();
+            con.Close();
+            clearData();
+            MessageBox.Show("book updated successfully");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
